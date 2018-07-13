1b1fd7873a3933b134e3b122486ea80c3deda5a0
suckthegithub <35280540+suckthegithub@users.noreply.github.com>
2018-07-13 14:31:48 +0800

    Update rc4.h

--- include/rc4.h
+++ include/rc4.h
@@ -5,11 +5,18 @@
 
 #ifndef rc4_h
 #define rc4_h
-
-#define RC4_TABLE_LENGTH 256
+#ifdef __cplusplus
+extern "C" {
+#endif
+#define RC4_TABLE_LENGTH 512
 
 void rc4_encrypt(unsigned char* schedule, unsigned char* data, unsigned char* dest, unsigned int length);
 void rc4_decrypt(unsigned char* schedule, unsigned char* data, unsigned char* dest, unsigned int length);
 void rc4_generate_key(unsigned char* schedule, const unsigned char* key, int key_length);
 
+#ifdef __cplusplus
+}
+#endif
+
 #endif /* rc4_h */
+
