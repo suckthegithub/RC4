a8d7071215b732a4287b802f1da895e3fb8acc30
whoami <35280540+suckthegithub@users.noreply.github.com>
2018-07-17 15:40:17 +0800

    Update rc4.h
    older fuck up

--- include/rc4.h
+++ include/rc4.h
@@ -1,7 +1,5 @@
 //  rc4.h
-//
-//  Created by Smith on 2017/07/31.
-//  Copyright © 2017 Smith. All rights reserved.
+
 
 #ifndef rc4_h
 #define rc4_h
@@ -10,10 +8,11 @@ extern "C" {
 #endif
 #define RC4_TABLE_LENGTH 256
 
-void rc4_encrypt(unsigned char* schedule, unsigned char* data, unsigned char* dest, unsigned int length);
-void rc4_decrypt(unsigned char* schedule, unsigned char* data, unsigned char* dest, unsigned int length);
-void rc4_generate_key(unsigned char* schedule, const unsigned char* key, int key_length);
+//gen key
+void rc4_init(unsigned char*s, unsigned char*key, unsigned long Len);
 
+//dec or enc
+void rc4_crypt(unsigned char*s, unsigned char*Data, unsigned long Len);
 #ifdef __cplusplus
 }
 #endif
